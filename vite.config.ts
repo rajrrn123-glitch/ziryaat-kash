49222191e36238567c5c0316634d2e6044743c7b
rajrrn123-glitch <rajrrn123@gmail.com>
2026-07-19 19:09:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base: "/ziryaat-kash/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
